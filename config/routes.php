00a33967e9e6cd2ca1e01e40f66264f75404ac44
Joao <j.a.c.buschinelli@gmail.com>
2024-07-20 17:05:37 -0300

    added dependencies and validator middleware

--- config/routes.php
+++ config/routes.php
@@ -13,6 +13,8 @@ use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\HttpController@index');
 
+Router::post('/register', 'App\Controller\AuthController@register');
+
 Router::get('/favicon.ico', function () {
     return '';
 });
